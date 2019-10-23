60ddcb04fc7b107066b403ab3b334ac0bb60a25a
Pavel Elfimov <pelfimov@yandex.ru>
2019-10-23 21:57:14 +0700

    to do

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import questions from './mocks/questions';
+
 
 function handleClick() {
 
 }
 
 const init = () => {
-  ReactDOM.render(
-      <App time={6} mistakes={3} onClick={handleClick} />,
-      document.querySelector(`#root`)
+  ReactDOM.render(<App
+    time={6}
+    mistakes={3}
+    onClick={handleClick}
+    questions={questions}
+  />, document.querySelector(`#root`)
   );
 };
 
