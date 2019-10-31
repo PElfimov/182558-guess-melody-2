85860ebe41a10cf7b788b46b068eae0b48ca97b0
Pavel <pelfimov@yandex.ru>
2019-10-31 08:46:37 +0700

    apdate mock

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -3,40 +3,40 @@ const questions = [
     type: `genre`,
     genre: `rock`,
     answers: [{
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      src: `https://ru-drivemusic.net/dl/jpv4eU0ki4-vtAQ5LTKgTA/1572529237/download_music/2019/10/simple-plan-state-champs-feat.-we-the-kings-where-i-belong.mp3`,
       genre: `rock`,
     },
     {
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      src: `https://ru-drivemusic.net/dl/3tDzJM8JvXPlhcr0TFTg4g/1572529295/download_music/2019/10/dante-streljajj.mp3`,
       genre: `pop`,
     },
     {
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      src: `https://ru-drivemusic.net/dl/s_pFV8JcEoR2JqsiTKfOhA/1572529325/download_music/2013/04/da-gudda-jazz-feat.-ljey-just-jazz-vse-prosto.mp3`,
       genre: `jazz`,
     },
     {
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      src: `https://ru-drivemusic.net/dl/30xQdqZr2ZsSiKo1KxZapg/1572529272/download_music/2013/12/we-the-kings-queen-of-hearts.mp3`,
       genre: `rock`,
     }],
   },
   {
     type: `genre`,
-    genre: `folk`,
+    genre: `dance`,
     answers: [
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://ru-drivemusic.net/dl/WniqyI2XkrvrXjC4u7AwdQ/1572529467/download_music/2019/10/bad-wolves-crying-game.mp3`,
         genre: `rock`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-        genre: `pop`,
+        src: `https://ru-drivemusic.net/dl/bRsXc5_eA_aCygFCDE6QMg/1572529402/download_music/2019/10/nola-glupoe-serdce-jonvs-remix.mp3`,
+        genre: `dance`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://ru-drivemusic.net/dl/xUgWrXmN6q3RDak4xv7c8g/1572529371/download_music/2013/04/da-gudda-jazz-feat.-ljey-just-jazz-vse-prosto.mp3`,
         genre: `jazz`,
       },
       {
-        src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+        src: `https://ru-drivemusic.net/dl/93cQKEFFLRCSf-FFC3c7uQ/1572529494/download_music/2016/07/zhenja-latyshev-feat.-vlad-gorban-tolko-ty.mp3`,
         genre: `rock`,
       },
     ],
@@ -44,13 +44,13 @@ const questions = [
   {
     type: `artist`,
     song: {
-      artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
+      artist: `JM Sicky feat. Eva Menson`,
+      src: `https://ru-drivemusic.net/dl/zLqDWPKRneNCKTU2O617Hw/1572529543/download_music/2011/07/alexis-dante-amp-jm-sicky-feat.-eva-menson-alive.mp3`,
     },
     answers: [
       {
         picture: `http://placehold.it/134x134`,
-        artist: `John Snow`,
+        artist: `JM Sicky feat. Eva Menson`,
       },
       {
         picture: `http://placehold.it/134x134`,
