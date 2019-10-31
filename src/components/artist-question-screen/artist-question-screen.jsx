3be6f317378afe31e6e29511c3433b2c1f85aec6
Pavel Elfimov <pelfimov@yandex.ru>
2019-10-31 21:03:50 +0700

    add player in artist question

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,68 +1,97 @@
 import React from "react";
 import PropTypes from "prop-types";
+import AudioPlayer from "../audio-player/audio-player";
 
-export default function ArtistQuestionScreen(props) {
-  const {onAnswer, screenIndex, question} = props;
-  const {answers} = question;
+export default class ArtistQuestionScreen extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <section className="game game--artist">
-      <header className="game__header">
-        <a className="game__back">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
+    this.state = {
+      isPlaying: false
+    };
 
-        <div className="timer__value">
-          <span className="timer__mins">05</span>
-          <span className="timer__dots">:</span>
-          <span className="timer__secs">00</span>
-        </div>
+    this._handlePlayButtonClick = this._handlePlayButtonClick.bind(this);
+  }
 
-        <div className="game__mistakes">
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-          <div className="wrong"></div>
-        </div>
-      </header>
+  _handlePlayButtonClick() {
+    this.setState({isPlaying: !this.state.isPlaying});
+  }
 
-      <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
-        <div className="game__track">
-          <div className="track">
-            <button className="track__button track__button--play" type="button" />
-            <div className="track__status">
-              <audio />
+  render() {
+    const {question, screenIndex, onAnswer} = this.props;
+    const {isPlaying} = this.state;
+    const {answers, song} = question;
+
+    return (
+      <section className="game game--artist">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+          </a>
+
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle
+              className="timer__line"
+              cx={390}
+              cy={390}
+              r={370}
+              style={{filter: `url(#blur)`, transform: `scaleY(-1)`, transformOrigin: `center`}}
+            />
+          </svg>
+
+          <div className="timer__value" xmlns="http://www.w3.org/1999/xhtml">
+            <span className="timer__mins">05</span>
+            <span className="timer__dots">:</span>
+            <span className="timer__secs">00</span>
+          </div>
+
+          <div className="game__mistakes">
+            <div className="wrong" />
+            <div className="wrong" />
+            <div className="wrong" />
+          </div>
+        </header>
+
+        <section className="game__screen">
+          <h2 className="game__title">Кто исполняет эту песню?</h2>
+          <div className="game__track">
+            <div className="track">
+              <AudioPlayer
+                isPlaying={isPlaying}
+                onPlayButtonClick={this._handlePlayButtonClick}
+                src={song.src}
+              />
             </div>
           </div>
-        </div>
 
-        <form
-          className="game__artist"
-          onChange={(evt) => {
-            onAnswer([JSON.parse(evt.target.value)]);
-          }}>
-          {answers.map((it, i) => {
-            return (
-              <div key={`${screenIndex}-answer-${i}`} className="artist">
-                <input
-                  className="artist__input visually-hidden"
-                  type="radio"
-                  name="answer"
-                  value={JSON.stringify(it)}
-                  id={`answer-${i}`}
-                />
-                <label className="artist__name" htmlFor={`answer-${i}`}>
-                  <img className="artist__picture" src={it.picture} alt={it.artist} />
-                  {it.artist}
-                </label>
-              </div>
-            );
-          })}
-        </form>
+          <form
+            className="game__artist"
+            onChange={(evt) => {
+              onAnswer([JSON.parse(evt.target.value)]);
+            }}>
+            {answers.map((answer, i) => {
+              return (
+                <div key={`${screenIndex}-answer-${i}`} className="artist">
+                  <input
+                    className="artist__input visually-hidden"
+                    type="radio"
+                    name="answer"
+                    value={JSON.stringify(answer)}
+                    id={`answer-${i}`}
+                  />
+                  <label className="artist__name" htmlFor={`answer-${i}`}>
+                    <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                    {answer.artist}
+                  </label>
+                </div>
+              );
+            })}
+          </form>
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
 }
 
 ArtistQuestionScreen.propTypes = {
@@ -79,6 +108,6 @@ ArtistQuestionScreen.propTypes = {
         })
     )
   }).isRequired,
-  onAnswer: PropTypes.func.isRequired,
-  screenIndex: PropTypes.number.isRequired
+  screenIndex: PropTypes.number.isRequired,
+  onAnswer: PropTypes.func
 };
