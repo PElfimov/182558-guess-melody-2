ffdca94571b9734f4a74831de9652cc15b05235a
Pavel Elfimov <pelfimov@yandex.ru>
2019-11-10 11:12:03 +0700

    reaf artist question screen to function comp

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,53 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default class ArtistQuestionScreen extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this._handlePlayButtonClick = this._handlePlayButtonClick.bind(this);
-  }
+const ArtistQuestionScreen = (props) => {
+  const {question, screenIndex, onAnswer, renderPlayer} = props;
+  const {answers, song} = question;
 
-  _handlePlayButtonClick() {
-    this.setState({isPlaying: !this.state.isPlaying});
-  }
-
-  render() {
-    const {question, screenIndex, onAnswer, renderPlayer} = this.props;
-    const {answers, song} = question;
-
-    return (
-      <section className="game__screen">
-        <h2 className="game__title">Кто исполняет эту песню?</h2>
-        <div className="game__track">
-          <div className="track">{renderPlayer(song, 0)}</div>
-        </div>
-        <form
-          className="game__artist"
-          onChange={(evt) => {
-            onAnswer([JSON.parse(evt.target.value)]);
-          }}>
-          {answers.map((answer, i) => {
-            return (
-              <div key={`${screenIndex}-answer-${i}`} className="artist">
-                <input
-                  className="artist__input visually-hidden"
-                  type="radio"
-                  name="answer"
-                  value={JSON.stringify(answer)}
-                  id={`answer-${i}`}
-                />
-                <label className="artist__name" htmlFor={`answer-${i}`}>
-                  <img className="artist__picture" src={answer.picture} alt={answer.artist} />
-                  {answer.artist}
-                </label>
-              </div>
-            );
-          })}
-        </form>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="game__screen">
+      <h2 className="game__title">Кто исполняет эту песню?</h2>
+      <div className="game__track">
+        <div className="track">{renderPlayer(song, 0)}</div>
+      </div>
+      <form
+        className="game__artist"
+        onChange={(evt) => {
+          onAnswer([JSON.parse(evt.target.value)]);
+        }}>
+        {answers.map((answer, i) => {
+          return (
+            <div key={`${screenIndex}-answer-${i}`} className="artist">
+              <input
+                className="artist__input visually-hidden"
+                type="radio"
+                name="answer"
+                value={JSON.stringify(answer)}
+                id={`answer-${i}`}
+              />
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
+            </div>
+          );
+        })}
+      </form>
+    </section>
+  );
+};
 
 ArtistQuestionScreen.propTypes = {
   question: PropTypes.exact({
@@ -67,3 +56,5 @@ ArtistQuestionScreen.propTypes = {
   onAnswer: PropTypes.func,
   renderPlayer: PropTypes.func.isRequired
 };
+
+export default ArtistQuestionScreen;
