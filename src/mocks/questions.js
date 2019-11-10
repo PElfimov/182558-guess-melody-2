a24b050658805ecb735a6453abd83569ec546471
Pavel Elfimov <pelfimov@yandex.ru>
2019-11-10 10:27:35 +0700

    fix mock

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -3,19 +3,19 @@ const questions = [
     type: `genre`,
     genre: `rock`,
     answers: [{
-      src: `http://cdnet4.mixmuz.ru/b941501145f7/4ede66792ff9/285012b2af405562cbc2d3bb2a7fe5d8-2ea34336-11f7c734-1-64652aad67d/%D0%A1%D0%BF%D0%BB%D0%B8%D0%BD%20%E2%80%94%20%D0%92%D1%8B%D1%85%D0%BE%D0%B4%D0%B0%20%D0%BD%D0%B5%D1%82.mp3`,
+      src: `https://ru-drivemusic.net/dl/jpv4eU0ki4-vtAQ5LTKgTA/1572529237/download_music/2019/10/simple-plan-state-champs-feat.-we-the-kings-where-i-belong.mp3`,
       genre: `rock`,
     },
     {
-      src: `http://cdnet4.mixmuz.ru/876ee31e472f/64259a564c8f/8505f6d50bd82f9f9ebccf3e7fe54539-1cca3d89-e315bd2-1-475515ca538/%D0%A4%D0%BE%D1%80%D0%BC%D0%B0%D1%82%20%E2%80%94%20%D0%BF%D0%BE%D0%B7%D0%B2%D0%BE%D0%BD%D0%B8%20%D0%BC%D0%BD%D0%B5.mp3`,
+      src: `https://ru-drivemusic.net/dl/3tDzJM8JvXPlhcr0TFTg4g/1572529295/download_music/2019/10/dante-streljajj.mp3`,
       genre: `pop`,
     },
     {
-      src: `http://cdnet4.mixmuz.ru/d09bf3232d95/e6edf85427f7/e245b642819013ce188aa987aca0bbd8-2c7e6b32-11f59b2c-1-12fd5bce2f60/Karen%20Souza%2C%20Stereo%20Dub%20%E2%80%94%20Safe%20and%20Sound.mp3`,
+      src: `https://ru-drivemusic.net/dl/s_pFV8JcEoR2JqsiTKfOhA/1572529325/download_music/2013/04/da-gudda-jazz-feat.-ljey-just-jazz-vse-prosto.mp3`,
       genre: `jazz`,
     },
     {
-      src: `http://cdnet4.mixmuz.ru/e3086e17b32a/68d1548d1465/285012b2af405562cbc2d3bb2a7fe5d8-3bb50986-11f6ab4a-1-e6fd38bdbc7/%D0%9A%D0%BE%D1%80%D0%BE%D0%BB%D1%8C%20%D0%B8%20%D0%A8%D1%83%D1%82%20%E2%80%94%20%D0%9B%D0%B5%D1%81%D0%BD%D0%B8%D0%BA.mp3`,
+      src: `https://ru-drivemusic.net/dl/30xQdqZr2ZsSiKo1KxZapg/1572529272/download_music/2013/12/we-the-kings-queen-of-hearts.mp3`,
       genre: `rock`,
     }],
   },
@@ -23,19 +23,19 @@ const questions = [
     type: `genre`,
     genre: `pop`,
     answers: [{
-      src: `http://cdnet4.mixmuz.ru/b941501145f7/4ede66792ff9/285012b2af405562cbc2d3bb2a7fe5d8-2ea34336-11f7c734-1-64652aad67d/%D0%A1%D0%BF%D0%BB%D0%B8%D0%BD%20%E2%80%94%20%D0%92%D1%8B%D1%85%D0%BE%D0%B4%D0%B0%20%D0%BD%D0%B5%D1%82.mp3`,
+      src: `https://ru-drivemusic.net/dl/jpv4eU0ki4-vtAQ5LTKgTA/1572529237/download_music/2019/10/simple-plan-state-champs-feat.-we-the-kings-where-i-belong.mp3`,
       genre: `rock`,
     },
     {
-      src: `http://cdnet4.mixmuz.ru/876ee31e472f/64259a564c8f/8505f6d50bd82f9f9ebccf3e7fe54539-1cca3d89-e315bd2-1-475515ca538/%D0%A4%D0%BE%D1%80%D0%BC%D0%B0%D1%82%20%E2%80%94%20%D0%BF%D0%BE%D0%B7%D0%B2%D0%BE%D0%BD%D0%B8%20%D0%BC%D0%BD%D0%B5.mp3`,
+      src: `https://ru-drivemusic.net/dl/3tDzJM8JvXPlhcr0TFTg4g/1572529295/download_music/2019/10/dante-streljajj.mp3`,
       genre: `pop`,
     },
     {
-      src: `http://cdnet4.mixmuz.ru/d09bf3232d95/e6edf85427f7/e245b642819013ce188aa987aca0bbd8-2c7e6b32-11f59b2c-1-12fd5bce2f60/Karen%20Souza%2C%20Stereo%20Dub%20%E2%80%94%20Safe%20and%20Sound.mp3`,
+      src: `https://ru-drivemusic.net/dl/s_pFV8JcEoR2JqsiTKfOhA/1572529325/download_music/2013/04/da-gudda-jazz-feat.-ljey-just-jazz-vse-prosto.mp3`,
       genre: `jazz`,
     },
     {
-      src: `http://cdnet4.mixmuz.ru/e3086e17b32a/68d1548d1465/285012b2af405562cbc2d3bb2a7fe5d8-3bb50986-11f6ab4a-1-e6fd38bdbc7/%D0%9A%D0%BE%D1%80%D0%BE%D0%BB%D1%8C%20%D0%B8%20%D0%A8%D1%83%D1%82%20%E2%80%94%20%D0%9B%D0%B5%D1%81%D0%BD%D0%B8%D0%BA.mp3`,
+      src: `https://ru-drivemusic.net/dl/30xQdqZr2ZsSiKo1KxZapg/1572529272/download_music/2013/12/we-the-kings-queen-of-hearts.mp3`,
       genre: `rock`,
     }],
   },
@@ -45,19 +45,19 @@ const questions = [
     genre: `dance`,
     answers: [
       {
-        src: `http://cdnet4.mixmuz.ru/136d9180b2781/4ea0cc07e92/285012b2af405562cbc2d3bb2a7fe5d8-2ea34329-11f73c3d-1-1778c617706e/%D0%97%D0%B5%D0%BC%D1%84%D0%B8%D1%80%D0%B0%20%E2%80%94%20%D0%98%D1%81%D0%BA%D0%B0%D0%BB%D0%B0.mp3`,
+        src: `https://ru-drivemusic.net/dl/WniqyI2XkrvrXjC4u7AwdQ/1572529467/download_music/2019/10/bad-wolves-crying-game.mp3`,
         genre: `rock`,
       },
       {
-        src: `http://cdnet4.mixmuz.ru/7715d75e8d20/1301b08590d65/a74d56d3e62f45a7e221af26f63adba8-3bb5099b-11f710f1-1-609eec1ee03/NILETTO%20%E2%80%94%20%D0%9B%D1%8E%D0%B1%D0%B8%D0%BC%D0%BA%D0%B0.mp3`,
+        src: `https://ru-drivemusic.net/dl/bRsXc5_eA_aCygFCDE6QMg/1572529402/download_music/2019/10/nola-glupoe-serdce-jonvs-remix.mp3`,
         genre: `dance`,
       },
       {
-        src: `http://cdnet4.mixmuz.ru/5a483062123c/d58b34fa073e/e245b642819013ce188aa987aca0bbd8-19dd6f5-11f59bb5-1-f18eb4b3383/Moon%20Hooch%2C%20Tonio%20Sagan%20%E2%80%94%20Acid%20Mountain.mp3`,
+        src: `https://ru-drivemusic.net/dl/xUgWrXmN6q3RDak4xv7c8g/1572529371/download_music/2013/04/da-gudda-jazz-feat.-ljey-just-jazz-vse-prosto.mp3`,
         genre: `jazz`,
       },
       {
-        src: `http://cdnet4.mixmuz.ru/3b671a30ccc4/126c393eb7db9/285012b2af405562cbc2d3bb2a7fe5d8-2ea34335-11f71ac2-1-156556e2a5d6/%D0%A2%D0%B0%D0%BD%D1%86%D1%8B%20%D0%9C%D0%B8%D0%BD%D1%83%D1%81%20%E2%80%94%20%D0%9F%D0%BE%D0%BB%D0%BE%D0%B2%D0%B8%D0%BD%D0%BA%D0%B0.mp3`,
+        src: `https://ru-drivemusic.net/dl/93cQKEFFLRCSf-FFC3c7uQ/1572529494/download_music/2016/07/zhenja-latyshev-feat.-vlad-gorban-tolko-ty.mp3`,
         genre: `rock`,
       },
     ],
@@ -66,7 +66,7 @@ const questions = [
     type: `artist`,
     song: {
       artist: `JM Sicky feat. Eva Menson`,
-      src: `http://cdnet4.mixmuz.ru/e771ba67e30e/fecd773341cb/6665f9097c87d8d243e4548bbb0f6575-2ea3434c-11f62fcf-1-abbb2f666b2/Linkin%20Park%20%E2%80%94%20Numb.mp3`,
+      src: `https://ru-drivemusic.net/dl/zLqDWPKRneNCKTU2O617Hw/1572529543/download_music/2011/07/alexis-dante-amp-jm-sicky-feat.-eva-menson-alive.mp3`,
     },
     answers: [
       {
@@ -89,19 +89,19 @@ const questions = [
     genre: `jazz`,
     answers: [
       {
-        src: `http://cdnet4.mixmuz.ru/136d9180b2781/4ea0cc07e92/285012b2af405562cbc2d3bb2a7fe5d8-2ea34329-11f73c3d-1-1778c617706e/%D0%97%D0%B5%D0%BC%D1%84%D0%B8%D1%80%D0%B0%20%E2%80%94%20%D0%98%D1%81%D0%BA%D0%B0%D0%BB%D0%B0.mp3`,
+        src: `https://ru-drivemusic.net/dl/WniqyI2XkrvrXjC4u7AwdQ/1572529467/download_music/2019/10/bad-wolves-crying-game.mp3`,
         genre: `rock`,
       },
       {
-        src: `http://cdnet4.mixmuz.ru/7715d75e8d20/1301b08590d65/a74d56d3e62f45a7e221af26f63adba8-3bb5099b-11f710f1-1-609eec1ee03/NILETTO%20%E2%80%94%20%D0%9B%D1%8E%D0%B1%D0%B8%D0%BC%D0%BA%D0%B0.mp3`,
+        src: `https://ru-drivemusic.net/dl/bRsXc5_eA_aCygFCDE6QMg/1572529402/download_music/2019/10/nola-glupoe-serdce-jonvs-remix.mp3`,
         genre: `dance`,
       },
       {
-        src: `http://cdnet4.mixmuz.ru/5a483062123c/d58b34fa073e/e245b642819013ce188aa987aca0bbd8-19dd6f5-11f59bb5-1-f18eb4b3383/Moon%20Hooch%2C%20Tonio%20Sagan%20%E2%80%94%20Acid%20Mountain.mp3`,
+        src: `https://ru-drivemusic.net/dl/xUgWrXmN6q3RDak4xv7c8g/1572529371/download_music/2013/04/da-gudda-jazz-feat.-ljey-just-jazz-vse-prosto.mp3`,
         genre: `jazz`,
       },
       {
-        src: `http://cdnet4.mixmuz.ru/3b671a30ccc4/126c393eb7db9/285012b2af405562cbc2d3bb2a7fe5d8-2ea34335-11f71ac2-1-156556e2a5d6/%D0%A2%D0%B0%D0%BD%D1%86%D1%8B%20%D0%9C%D0%B8%D0%BD%D1%83%D1%81%20%E2%80%94%20%D0%9F%D0%BE%D0%BB%D0%BE%D0%B2%D0%B8%D0%BD%D0%BA%D0%B0.mp3`,
+        src: `https://ru-drivemusic.net/dl/93cQKEFFLRCSf-FFC3c7uQ/1572529494/download_music/2016/07/zhenja-latyshev-feat.-vlad-gorban-tolko-ty.mp3`,
         genre: `rock`,
       },
     ],
